c345d995c4b63eda2054503776a669e27b8ab15a
KatieONell <katicusonellicus@gmail.com>
2023-06-19 14:08:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import numpy as np
 @st.cache_resource
 def get_model():
   return SentenceTransformer('all-mpnet-base-v2')
+
   
 placeholder = st.empty()
 
@@ -14,14 +15,13 @@ with placeholder.container():
   st.image("https://i.imgur.com/VeuZ9cG.gif", width=700)
   form = st.form(key='my_form')
   interp = form.text_input(label="What is the relationship between these two people? Who are they to each other?", key='samplekey')
+  embedding = ''
   submit_button = form.form_submit_button(label='Submit')
   if submit_button:
-    model = get_model()
-    embedding = model.encode([interp])
     placeholder.empty()
-
-    st.write(interp)
-    st.write(str(embedding))
+    embedding = get_model().encode([interp])
+  st.write(interp)
+  st.write(str(embedding))
 
 
 
