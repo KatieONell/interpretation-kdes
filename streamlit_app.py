e886994e7fbfd67a404a3295275ee9362a4f27df
KatieONell <katicusonellicus@gmail.com>
2023-06-19 13:59:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ placeholder = st.empty()
 with placeholder.container():
   st.image("https://i.imgur.com/VeuZ9cG.gif", width=700)
   form = st.form(key='my_form')
-  interp = form.text_input(label="What is the relationship between these two people? Who are they to each other?")
+  interp = form.text_input(label="What is the relationship between these two people? Who are they to each other?", key='samplekey')
   submit_button = form.form_submit_button(label='Submit')
   if submit_button:
     #model = get_model()
