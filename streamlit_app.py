5a5c6e997ebdd7e2d16bd9ec91375538d56de55c
KatieONell <katicusonellicus@gmail.com>
2023-06-19 13:12:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
-clicked = st.button("submit")
-while not clicked:
-  st.image("https://i.imgur.com/VeuZ9cG.gif", width=700)
-  st.text_input("What is the relationship between these two people? Who are they to each other?")
-st.write('new section')
+placeholder1 = st.empty()
+placeholder2 = st.empty()
+placeholder3 = st.empty()
+
+placeholder1.image("https://i.imgur.com/VeuZ9cG.gif", width=700)
+placeholder2.text_input("What is the relationship between these two people? Who are they to each other?")
+placeholder3.button("submit")
+if placeholder3:
+  placeholder1.empty()
+  placeholder2.empty()
+  placeholder3.empty()
+  
+  
