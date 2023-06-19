dcf61f9a6f9b700aa86971998362c79a75815853
KatieONell <katicusonellicus@gmail.com>
2023-06-19 13:07:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-
-st.image("https://i.imgur.com/VeuZ9cG.gif", width=400)
-st.text_area("What is the relationship between these two people? Who are they to each other?")
+clicked = st.button("submit")
+while not clicked:
+  st.image("https://i.imgur.com/VeuZ9cG.gif", width=700)
+  st.text_input("What is the relationship between these two people? Who are they to each other?")
+st.write('new section')
