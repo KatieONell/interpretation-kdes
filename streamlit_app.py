961d15c66ba745099dc6ffdb8dfb635877eb2b71
KatieONell <katicusonellicus@gmail.com>
2023-06-19 13:21:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 placeholder = st.empty()
 
-with placeholder.continer():
+with placeholder.container():
   st.image("https://i.imgur.com/VeuZ9cG.gif", width=700)
   interp = st.text_input("What is the relationship between these two people? Who are they to each other?")
   clicked = st.button("submit")
